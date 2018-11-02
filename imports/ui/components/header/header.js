5c953fc3cb98d3ffa2fc9115d89b9f2037be7bcf
Gregor Schumann <gregor.schumann@sap.com>
2018-11-02 16:41:48 +0100

    add header and sitemenu

---
+++ imports/ui/components/header/header.js
@@ -0,0 +1,15 @@
+import './header.html';
+
+Template.header.events({
+    'click #menuOpen'(event, instance) {
+        var x = document.getElementById("mySidebar");
+        x.style.width = "100%";
+        x.style.fontSize = "40px";
+        x.style.paddingTop = "10%";
+        x.style.display = "block";
+    },
+
+    'click #menuClose'(event, instance) {
+        document.getElementById("mySidebar").style.display = "none";
+    },
+  });
--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,4 +1,6 @@
 import './home.html';
 
+import '../../components/header/header.js';
+
 import '../../components/hello/hello.js';
 import '../../components/info/info.js';
