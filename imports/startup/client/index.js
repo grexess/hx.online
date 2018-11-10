2fdeb8ed07214febd84c9f91b6c48568b96e176c
grexess <grexess@googlemail.com>
2018-11-10 14:00:21 +0100

    change header

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,3 +1,12 @@
 // Import client startup through a single index entry point
 
 import './routes.js';
+
+
+Accounts.onLogin(function(user){
+    if(user.type === "password"){
+        $('#id01').hide();
+        
+    }
+    $('#userName').text(Meteor.user().emails[0].address);
+  });
